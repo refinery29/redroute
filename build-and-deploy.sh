61a3bc7a7a5ebf39ac2245f0fa5787e8a3e97df3
Schlueter <b@schlueter.blue>
2019-07-15 16:56:52 -0700

    Only include dependencies in package for lambda

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,3 +1,17 @@
-npm ci
-zip -r lambda-function-package.zip node_modules index.js
+#!/bin/sh
+#npm ci
+enumerate_package_dependencies () {
+    for package in $(jq -r '.dependencies|keys[]' node_modules/$1/package.json 2>/dev/null)
+    do
+        enumerate_package_dependencies "$package"
+    done
+    echo "$1"
+}
+zip lambda-function-package.zip index.js
+
+for package in $(enumerate_package_dependencies $(jq -r '.dependencies| keys[]' package.json))
+do
+    zip -r lambda-function-package.zip node_modules/$package
+done
+
 aws lambda update-function-code --function-name redroute --zip-file fileb://lambda-function-package.zip
