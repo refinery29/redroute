e0065e8e80c068b0d08e251178d4021b2237c2ee
Schlueter <b@schlueter.blue>
2019-07-26 11:06:51 -0700

    Parameterize function name

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
+[ "$debug" = 'true' ] && set -x
 set -e
+
+FUNCTION_SUFFIX="$1"
+ZIPFILE="${TMPDIR:-/tmp/}lambda-function-package-$(date +%s).zip"
+
 npm ci
 npm run lint
-ZIPFILE="${TMPDIR:-/tmp/}lambda-function-package-$(date +%s).zip"
+
 enumerate_package_dependencies () {
     for package in $(jq -r '.dependencies|keys[]' "node_modules/$1/package.json" 2>/dev/null)
     do
@@ -10,6 +16,7 @@ enumerate_package_dependencies () {
     done
     echo "$1"
 }
+
 zip "$ZIPFILE" index.js
 
 for package in $(enumerate_package_dependencies "$(jq -r '.dependencies| keys[]' package.json)")
@@ -17,4 +24,4 @@ do
     zip -r "$ZIPFILE" "node_modules/$package"
 done
 
-aws lambda update-function-code --function-name redroute --zip-file "fileb://$ZIPFILE"
+aws lambda update-function-code --function-name "redroute-${FUNCTION_SUFFIX}" --zip-file "fileb://$ZIPFILE"
