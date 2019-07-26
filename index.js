6212eee2715c7df0e7eebf756e3b625b248c01f8
johnnyshankman <john.l.shankman@gmail.com>
2019-07-26 11:51:58 -0400

    Fix callback() and event loop related issues

--- index.js
+++ index.js
@@ -3,80 +3,81 @@ const redisPort = process.env.REDIS_PORT || 6379;
 const redis = require('redis');
 
 exports.handler = (event, context, callback) => {
+  /*
+   * Because Redis leaves an open socket, the event loop will always have something in it.
+   * Lets not let that get in our way and cause needless timeouts.
+   */
+  context.callbackWaitsForEmptyEventLoop = false;
+
   const redisClient = redis.createClient({
     host: redisHost,
     port: redisPort
+  });
+
+  redisClient.on('error', (err) => {
+    callback(new Error(err));
   });
 
   const commands = {
     'hset': (key, field, value) => {
       redisClient.hset(key, field, value, (err) => {
         if (err) {
-          callback(Error(err));
-        } else {
-          // Query for new key to confirm its creation, we may not need this
-          redisClient.hget(key, field, (err, value) => {
-            if (err) {
-              callback(Error(err));
-            } else {
-              callback(null, 'HSET ' + key + ' ' + field + ' ' + value);
-            }
-          });
+          callback(new Error(err));
         }
+
+        // Query for new key to confirm its creation, we may not need this
+        redisClient.hget(key, field, (err, value) => {
+          if (err) {
+            callback(new Error(err));
+          }
+
+          callback(null, 'HSET SUCCESS ' + key + ' ' + field + ' ' + value);
+        });
       });
     },
     'hdel': (key, field) => {
       redisClient.hdel(key, field, (err) => {
         if (err) {
           callback(Error(err));
-        } else {
-          // Query for deleted key to confirm its deletion, we may not need this
-          redisClient.hget(key, field, (err, value) => {
-            if (err) {
-              callback(Error(err));
-            } else if (value !== null) {
-              callback(Error('Key failed to delete, value was ' + value));
-            } else {
-              callback(null, 'HDEL ' + key + ' ' + field);
-            }
-          });
         }
+
+        // Query for deleted key to confirm its deletion, we may not need this
+        redisClient.hget(key, field, (err, value) => {
+          if (err) {
+            callback(new Error(err));
+          } else if (value !== null) {
+            callback(new Error('Key failed to delete, value was ' + value));
+          }
+
+          callback(null, 'HDEL SUCCESS ' + key + ' ' + field);
+        });
       });
     }
   };
 
   console.log('Processing ' + event.Records.length + ' records');
-  redisClient.on('error', (err) => {
-    callback(Error(err));
-  });
 
-  for (var i = 0; i < event.Records.length; i++) {
-    const body = JSON.parse(event.Records[i].body);
-    const command = body.command;
-    const key = body.key;
-    const field = body.field;
-    const value = body.value;
-
-    console.log('processing command: ' + command +
-                  '\nkey: ' + key +
-                  '\nfield: ' + field +
-                  '\nvalue: ' + value);
-
+  event.Records.forEach((record) => {
     try {
+      const body = JSON.parse(record.body);
+      const command = body.command;
+      const key = body.key;
+      const field = body.field;
+      const value = body.value;
+
+      console.log('Processing command: ', command)
+      console.log('Key: ', key)
+      console.log('Field: ', field)
+      console.log('Value: ', value)
+
       // Call delegator function which can handle these early redis commands which share argument order.
       commands[command](key, field, value);
     } catch (err) {
       if (err instanceof TypeError) {
-        callback(Error('Command ' + command + ' not yet implemented'));
+        callback(Error('Command Not Found'));
       } else {
         callback(Error('Unknown error: ' + err.message));
       }
     }
-
-    // Ack for record would go here on fifo.
-    console.log('finished with ' + command +
-                ' ' + key +
-                ' ' + field +
-                ' ' + value);
-  }
+  });
 };
