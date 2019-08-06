d350cc9e1323aa6321b3f6fb44f0e35712e71048
Schlueter <b@schlueter.blue>
2019-08-06 10:50:48 -0700

    Make build script include all dependencies in deploy; Add comments to build script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -16,6 +16,8 @@ ZIPFILE="${TMPDIR:-/tmp/}lambda-function-package-$(date +%s).zip"
 npm ci
 npm run lint
 
+# From the package provided as an argument, which has been installed into the node_modules directory,
+# recursively get it's dependencies, and their dependencies, and so on, printing the name of each package.
 enumerate_package_dependencies () {
     for package in $(jq -r '.dependencies|keys[]' "node_modules/$1/package.json" 2>/dev/null)
     do
@@ -26,9 +28,13 @@ enumerate_package_dependencies () {
 
 zip "$ZIPFILE" index.js
 
-for package in $(enumerate_package_dependencies "$(jq -r '.dependencies| keys[]' package.json)")
+
+for top_level_dependency in $(jq -r '.dependencies| keys[]' package.json)
 do
-    zip -r "$ZIPFILE" "node_modules/$package"
+    for package in $(enumerate_package_dependencies "$top_level_dependency")
+    do
+        zip -r "$ZIPFILE" "node_modules/$package"
+    done
 done
 
 aws lambda update-function-code --function-name "redroute-${FUNCTION_SUFFIX}" --zip-file "fileb://$ZIPFILE"
